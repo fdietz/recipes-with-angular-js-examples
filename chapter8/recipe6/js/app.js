b4d65f2a9a6b93b30d32b0238b0dd6dcc795ca7a
Frederik Dietz <fdietz@gmail.com>
2013-02-19 17:00:00 +0100

    contenteditable recipe

---
+++ chapter8/recipe6/js/app.js
@@ -0,0 +1,24 @@
+var app = angular.module("MyApp", []);
+
+app.directive("contenteditable", function() {
+  return {
+    restrict: "A",
+    require: "?ngModel",
+    link: function(scope, element, attrs, ngModel) {
+
+      function read() {
+        ngModel.$setViewValue(element.html());
+      };
+
+      ngModel.$render = function() {
+        element.html(ngModel.$viewValue || "");
+      };
+
+      element.bind("blur keyup change", function() {
+        scope.$apply(read);
+      });
+
+      read();
+    }
+  };
+});
