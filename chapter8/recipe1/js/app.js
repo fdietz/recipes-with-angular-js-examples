da48ff32f1434781832a05644cccafa82e8584ef
Frederik Dietz <fdietz@gmail.com>
2013-02-19 12:04:16 +0100

    more complete example

--- chapter8/recipe1/js/app.js
+++ chapter8/recipe1/js/app.js
@@ -1,14 +1,17 @@
 var app = angular.module("MyApp", []);
 
 app.controller("MyCtrl", function($scope) {
-
   $scope.friends = [
-    { name: "Peter" },
-    { name: "Pablo" },
-    { name: "Linda" },
-    { name: "Marta" },
-    { name: "Othello" },
-    { name: "Markus" }
+    { name: "Peter",   age: 20 },
+    { name: "Pablo",   age: 55 },
+    { name: "Linda",   age: 20 },
+    { name: "Marta",   age: 37 },
+    { name: "Othello", age: 20 },
+    { name: "Markus",  age: 32 }
   ];
+
+  $scope.filterFunction = function(element) {
+    return element.name.match(/^Ma/) ? true : false;
+  };
 
 });
