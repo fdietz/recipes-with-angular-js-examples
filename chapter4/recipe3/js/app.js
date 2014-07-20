1f8ca5709cd67df5adf67b8f5dafa3ec296e851b
George Zografos <gpzog1991@gmail.com>
2014-07-20 03:05:14 +0300

    Changed recipe3 and added recipe5 in chapter 4.
    "Recipe3 - Passing Configuration Params to Filters" as described in
    the book does not match the repo's source code.
    
    "Recipe5 - Chaining Filters together" is missing. Even though the author
    explicitly says "I leave the implementation of the sortAscending filter
    as an exercise to the reader...", the source code of the recipe should be
    in the repo. Just like the rest of the examples.
    
    Kept the implementation of the sortAscendingFilter as simple as possible.
    
    The previous recipe3 is now recipe4.
    The previous recipe4 is now recipe6.

--- chapter4/recipe3/js/app.js
+++ chapter4/recipe3/js/app.js
@@ -1,13 +1,17 @@
 var app = angular.module("MyApp", []);
 
-app.filter("exclude", function() {
-  return function(input, exclude) {
-    var result = [];
+app.filter("reverse", function() {
+  return function(input, options) {
+    input = input || "";
+    var result = "";
+    var suffix = options["suffix"] || "";
+
     for (var i=0; i<input.length; i++) {
-      if (input[i] !== exclude) {
-        result.push(input[i]);
-      }
+      result = input.charAt(i) + result;
     }
+
+    if (input.length > 0) result += suffix;
+
 
     return result;
   };
